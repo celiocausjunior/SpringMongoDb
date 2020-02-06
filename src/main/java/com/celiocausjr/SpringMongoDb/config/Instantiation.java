3a42ce0b79f363691c650c93449c5c23e46b65a0
Celio Caus Junior <celiocausjunior@gmail.com>
2020-02-05 21:23:39 -0300

    Instanciacao da base de dados MongoDB

---
+++ src/main/java/com/celiocausjr/SpringMongoDb/config/Instantiation.java
@@ -0,0 +1,31 @@
+package com.celiocausjr.SpringMongoDb.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.celiocausjr.SpringMongoDb.domain.User;
+import com.celiocausjr.SpringMongoDb.repositories.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		userRepository.deleteAll();
+
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+	}
+
+}
