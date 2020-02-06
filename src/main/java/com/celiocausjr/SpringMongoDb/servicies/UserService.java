7bf2feb1cb96b012de53cbb0cbecdd524a4613ad
Celio Caus Junior <celiocausjunior@gmail.com>
2020-02-06 20:25:34 -0300

    Find By Id

--- src/main/java/com/celiocausjr/SpringMongoDb/servicies/UserService.java
+++ src/main/java/com/celiocausjr/SpringMongoDb/servicies/UserService.java
@@ -1,12 +1,14 @@
 package com.celiocausjr.SpringMongoDb.servicies;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.celiocausjr.SpringMongoDb.domain.User;
 import com.celiocausjr.SpringMongoDb.repositories.UserRepository;
+import com.celiocausjr.SpringMongoDb.servicies.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -19,4 +21,8 @@ public class UserService {
 
 	}
 
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
 }
