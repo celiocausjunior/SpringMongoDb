f837f679b436967f426daf90586a293698975abb
Celio Caus Junior <celiocausjunior@gmail.com>
2020-02-05 21:37:11 -0300

    DTO para retornar usuarios

--- src/main/java/com/celiocausjr/SpringMongoDb/resources/UserResource.java
+++ src/main/java/com/celiocausjr/SpringMongoDb/resources/UserResource.java
@@ -1,8 +1,7 @@
 package com.celiocausjr.SpringMongoDb.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.celiocausjr.SpringMongoDb.domain.User;
+import com.celiocausjr.SpringMongoDb.dto.UserDto;
 import com.celiocausjr.SpringMongoDb.servicies.UserService;
 
 @RestController
@@ -19,11 +19,12 @@ public class UserResource {
 
 	@Autowired
 	private UserService service;
-	
+
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDto>> findAll() {
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDto> listDto = list.stream().map(x -> new UserDto(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 
 }
